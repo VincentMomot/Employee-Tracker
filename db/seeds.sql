2035f04770b6eccf74e155b212e5845ce8e5c923
Vince Momot <vincemomot@gmail.com>
2021-11-14 20:33:20 -0500

    added a schema file and started the seed file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (department_id)
+VALUES ("1","2","3");
+INSERT INTO department (department_name)
+VALUES ("Engineering","Sales","Testing");
+
+INSERT INTO role (role_id)
+VALUES ("1","2","3","4","5","6");
+INSERT INTO role (role_title)
+VALUES ("Electrical Engineering","Mechanical Engineering","Finaning","Accounting","Quality Assurance","Production/Assembly");
+INSERT INTO role (role_salary)
+VALUES (100,000),(100,000),(75,000),(75,000),(65,000),(70,000);
+

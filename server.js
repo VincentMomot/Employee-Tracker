5bb595cbc9e531424477d950827b653dcf224071
Vince Momot <vincemomot@gmail.com>
2021-11-20 12:27:36 -0500

    all functions are working for MVP

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ function tesla() {
         "Add a role",
         "Add an employee",
         "Update an employee role",
-        "Quit program",
-        "TEST"
+        "Quit program"
       ]
     }
   ])
@@ -60,17 +59,7 @@ function tesla() {
           console.table(results);
         })
         tesla()
-      }
 
-      else if (answers.operation == "TEST") {
-        db.query(`SELECT *
-        FROM employee E
-        LEFT JOIN employee M
-        ON E.id = M.manager_id;`, function (err, results) {
-          console.log("\n");
-          console.table(results);
-        })
-        tesla()
       }
       else if (answers.operation == "View all employees") {
         db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS title, role.salary AS salary, department.name AS department, CONCAT(manager.first_name,' ',manager.last_name) AS manager_name
@@ -104,7 +93,7 @@ function tesla() {
       }
       else if (answers.operation == "Add a role") {
         addRole()
-        //need tesla call
+
       }
       else if (answers.operation == "Add an employee") {
         newEmployee()
@@ -132,22 +121,24 @@ app.listen(PORT, () => {
 
 const roleSelect = [];
 const managerSelect = [];
+
 function newEmployee() {
-  db.query(`SELECT title from role`, function (err, results) {
-    console.table(results)
+  db.query(`SELECT title AS name, id from role`, function (err, results) {
+    //console.table(results)
     let roles = results
     roles.map(role => {
       roleSelect.push(role)
 
     });
-    db.query(`SELECT first_name,last_name,first_name+''+last_name AS name from employee`, function (err, results) {
-      console.table(results)
+    db.query(`SELECT CONCAT(first_name,' ',last_name) AS name, id from employee`, function (err, results) {
+      //console.table(results)
       let managers = results
       managers.map(employee => {
         managerSelect.push(employee)
 
       });
-
+      //console.log(roleSelect)
+      //console.log(managerSelect)
       inquirer.prompt([{
         type: "input",
         message: "What is the employee's first name?",
@@ -159,70 +150,88 @@ function newEmployee() {
         name: "lName"
       },
       {
-        //this should take from the db and be a rawlist USE THIS TO FILL IN DEPARTMENT ALSO
         type: "list",
         message: "What is the employee's role?",
         name: "role",
         choices: roleSelect
       },
       {
-        //this should take from the db and be a rawlist
         type: "list",
         message: "Who is the employee's manager?",
         name: "manager",
         choices: managerSelect
+
       }
 
       ]).then(function (answers) {
-        db.query(`INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("${answers.fName}",${answers.lName},${answers.role},${answers.manager})`, function (err, results) {
+        const selectedRole = roleSelect.find(element => element.name == answers.role)
+        const selectedManager = managerSelect.find(element => element.name == answers.manager)
+        db.query(`INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES ("${answers.fName}","${answers.lName}",${selectedRole.id},${selectedManager.id})`, function (err, results) {
           console.log("\n");
           console.table(results);
         })
+        console.log("\n")
+        tesla()
       })
+
     })
+
   })
+  
 }
 
 const employeeSelect = [];
 
 function updateEmployee() {
-  db.query(`SELECT employee.first_name, employee.last_name FROM employee`, function (err, results) {
+  db.query(`SELECT CONCAT(first_name," ",last_name) AS name, id FROM employee`, function (err, results) {
     // console.table(results)
     let employees = results
     employees.map(employee => {
       employeeSelect.push(employee)
 
     });
+    db.query(`SELECT title AS name, id from role`, function (err, results) {
+      //console.table(results)
+      let roles = results
+      roles.map(role => {
+        roleSelect.push(role)
 
-    inquirer.prompt([
-      {
-        //this should take from the db and be a rawlist
-        type: "list",
-        message: "Which employee's role do you want to change?",
-        name: "name",
-        choices: employeeSelect
-      },
-      {
-        //this should take from the db and be a rawlist
-        type: "input",
-        message: "What do you want to change the employee's role to?",
-        name: "role"
-      }
-    ])
-      //display first and last name, but use ID for reference to change the employee's role
-      .then(function (answers) {
-        db.query(`UPDATE employee set employee WHERE id = ("${answers}")`, function (err, results) {
-          console.log("\n");
-          console.log(results);
+      });
+
+      inquirer.prompt([
+        {
+          //this should take from the db and be a rawlist
+          type: "list",
+          message: "Which employee's role do you want to change?",
+          name: "name",
+          choices: employeeSelect
+        },
+        {
+          //this should take from the db and be a rawlist
+          type: "list",
+          message: "What do you want to change the employee's role to?",
+          name: "role",
+          choices: roleSelect
+        }
+      ])
+        //display first and last name, but use ID for reference to change the employee's role
+        .then(function (answers) {
+          const selectedRole = roleSelect.find(element => element.name == answers.role)
+          const selectedEmployee = employeeSelect.find(element => element.name == answers.name)
+          db.query(`UPDATE employee SET role_id = ${selectedRole.id} WHERE id = ${selectedEmployee.id}`, function (err, results) {
+            console.log("\n");
+            console.log(results);
+          })
         })
-      })
-  })
+    })
+  });
+  tesla();
 };
 
 const departmentSelect = [];
 
 function addRole() {
-  db.query(`SELECT name from department`, function (err, results) {
+  db.query(`SELECT name,id from department`, function (err, results) {
     //console.table(results)
     let departments = results
     departments.map(department => {
@@ -230,7 +239,7 @@ function addRole() {
 
     });
 
-    
+
     //console.log(departmentSelect) 
     inquirer.prompt([
       {
@@ -251,20 +260,12 @@ function addRole() {
       }
     ])
       .then(function (answers) {
-        let deptNum = 0;
-        db.query(`SELECT department.id 
-                    FROM department
-                    WHERE department.name = "${answers.departmentSelect}"`), function (err, results) {
-            deptNum = results;
-            console.log(deptNum)
-            console.log(err)
-            
-
-          }
-        db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${answers.role}",${answers.salary},${deptNum})`, function (err, results) {
+        const selectedDept = departmentSelect.find(element => element.name == answers.department)
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES 
+         ("${answers.role}",${answers.salary},${selectedDept.id})`, function (err, results) {
           console.log("\n");
-
         })
+
         tesla();
       })
 
